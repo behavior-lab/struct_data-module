90ddd711d4e6ef630fb21d5f88abdd570a9d451a
Claus Hjort Bube <chb@b-cph.com>
2022-06-28 19:49:19 +0200

    Fixed slug name for extension stream

--- migrations/2022_06_13_084615_behavior_lab.module.struct_data__create_extensions_stream.php
+++ migrations/2022_06_13_084615_behavior_lab.module.struct_data__create_extensions_stream.php
@@ -35,7 +35,7 @@ class BehaviorLabModuleStructDataCreateExtensionsStream extends Migration
      * @var array
      */
     protected $stream = [
-        'slug' => 'structured_data',
+        'slug' => 'extensions',
     ];
 
     /**
